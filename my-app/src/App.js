f30001f9adf7d48abfabfa2822d218e1454f8ed5
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-05-23 22:49:38 +0300

    finished setting up to allow react to work

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+      My name is Mercy Ogalo
+     </h1>
+    </div>
+  );
+}
+
+export default App;
